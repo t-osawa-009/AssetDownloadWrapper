bdcae00e821a0781e0ebd02b75e81e919e8085a9
t-osawa-009 <da87435@gmail.com>
2020-04-23 15:19:51 +0900

    Add First commit

---
+++ AssetDownloadWrapper/AssetDownloadWrapper.h
@@ -0,0 +1,19 @@
+//
+//  AssetDownloadWrapper.h
+//  AssetDownloadWrapper
+//
+//  Created by Takuya Ohsawa on 2020/04/23.
+//  Copyright © 2020 Takuya Ohsawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssetDownloadWrapper.
+FOUNDATION_EXPORT double AssetDownloadWrapperVersionNumber;
+
+//! Project version string for AssetDownloadWrapper.
+FOUNDATION_EXPORT const unsigned char AssetDownloadWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetDownloadWrapper/PublicHeader.h>
+
+
