a83f71a0b9bc21074831fed52ee561d13c4935fd
t-osawa-009 <da87435@gmail.com>
2020-04-24 21:04:48 +0900

    Update settings

---
+++ AssetDownloadWrapper-Sample/AssetDownloadWrapper-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssetDownloadWrapper-Sample
+//
+//  Created by Takuya Ohsawa on 2020/04/24.
+//  Copyright © 2020 Takuya Ohsawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
